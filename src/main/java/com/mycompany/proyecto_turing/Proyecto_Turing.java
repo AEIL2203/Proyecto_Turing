4e7666f8dece0c21f1cd31e6694f45bc42c6e148
AEIL2203 <140118965+AEIL2203@users.noreply.github.com>
2024-10-26 00:28:30 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto_turing/Proyecto_Turing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_turing;
+
+/**
+ *
+ * @author angel
+ */
+public class Proyecto_Turing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
